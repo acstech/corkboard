299cf777e5b13827ea4fdd61b3173ede9918f16c
jasonm <jason.moore2@furman.edu>
2017-06-09 15:47:12 -0400

    Still debugging

--- main.go
+++ main.go
@@ -8,18 +8,19 @@ import (
 	"github.com/julienschmidt/httprouter"
 )
 
+//Index ...
 func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	fmt.Fprint(w, "Welcome!\n")
 }
 
+//Hello ...
 func Hello(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-	fmt.Fprint(w, "Hello, %s!\n", ps.ByName("name"))
+	fmt.Fprintf(w, "Hello, %s!\n", ps.ByName("name"))
 }
 
 func main() {
 	router := httprouter.New()
 	router.GET("/", Index)
 	router.GET("/hello/:name", Hello)
-
 	log.Fatal(http.ListenandServe(":8080", router))
 }
