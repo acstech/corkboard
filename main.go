a725fdc632cedef069d01961b869cc4c89c5b16c
jasonm <jason.moore2@furman.edu>
2017-06-09 16:02:48 -0400

    Removed a comment

--- main.go
+++ main.go
@@ -8,7 +8,6 @@ import (
 	"github.com/julienschmidt/httprouter"
 )
 
-//adding this to do a check
 //Index ...
 func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	fmt.Fprint(w, "Welcome!\n")
@@ -23,5 +22,5 @@ func main() {
 	router := httprouter.New()
 	router.GET("/", Index)
 	router.GET("/hello/:name", Hello)
-	log.Fatal(http.ListenandServe(":8080", router))
+	log.Fatal(http.ListenAndServe(":8080", router))
 }
