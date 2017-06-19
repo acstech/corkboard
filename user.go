523403d5c9fedd26edfcc42a35f854f0e59e13c9
jasonm <jason.moore2@furman.edu>
2017-06-19 15:29:02 -0400

    init and populate govendor, fixed linter issues from jason/createRestResources

--- user.go
+++ user.go
@@ -19,18 +19,6 @@ type User struct {
 	//Itemlist
 }
 
-type NewUserReq struct {
-	firstname string `json:"firstname"`
-	lastname  string `json:"lastname"`
-	email     string `json:"email"`
-	phone     string `json:"phone,omitempty"`
-}
-
-//User2 is uses for querying and adding fields
-type User2 struct {
-	User User `json:"user"`
-}
-
 /*Eventually want to change this method to be findUser and determine what the
 search key and return typeare using passed input. Currently, the query
 is hardcoded and the return type is a slice containing all the users currently
@@ -62,15 +50,7 @@ func (cb *Corkboard) findUsers() ([]User, error) {
 
 //findUserByID ...
 func (cb *Corkboard) findUserByID(id string) (*User, error) {
-	//Neither of the injected elements come from user input,
-	//should be relatively safe
 
-	// query := gocb.NewN1qlQuery(fmt.Sprintf("SELECT email, firstname, id, lastname, phone FROM `%s` WHERE _type = 'User' AND id = '%s'", cb.Bucket.Name(), id)) //nolint: gas
-	// res, err := cb.Bucket.ExecuteN1qlQuery(query, []interface{}{id})
-	// if err != nil {
-	// 	return nil, err
-	// }
-	// defer res.Close() //nolint: errcheck
 	//TODO: Make sure there is a user found by that id or throw error
 	key := "user:" + id
 	user := new(User)
