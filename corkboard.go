799cf3ec73b62fa8e4821ce6fbb45c11b6b25b5b
jasonm <jason.moore2@furman.edu>
2017-06-22 14:31:40 -0400

    Auth Middleware on all endpoints, clean up comments, update README

--- corkboard.go
+++ corkboard.go
@@ -13,8 +13,6 @@ import (
 type Corkboard struct {
 	Bucket        *gocb.Bucket
 	CorkboardAuth *corkboardauth.CorkboardAuth
-	//SiteID? Or is this auto-generated?
-
 }
 
 //CBConfig is all the necessary input values to configure a new CB Connection
@@ -32,7 +30,6 @@ func NewCorkboard(config *CBConfig) (*Corkboard, error) {
 		return nil, err
 	}
 	log.Println("Able to connect!")
-	//Connection opens successful
 	bucket, err := cluster.OpenBucket(config.BucketName, config.BucketPass)
 	if err != nil {
 		return nil, err
@@ -49,28 +46,23 @@ func NewCorkboard(config *CBConfig) (*Corkboard, error) {
 		return nil, err
 	}
 	return &Corkboard{
-		Bucket: bucket, CorkboardAuth: cba}, nil //no error if we get this far
+		Bucket: bucket, CorkboardAuth: cba}, nil
 
 }
 
-//Router returns all the router containing the Corkboard endpoints
+//Router returns the router containing the Corkboard endpoints
 func (cb *Corkboard) Router() *httprouter.Router {
 	router := httprouter.New()
 	stdChain := madhatter.New(cb.authToken)
 
-	router.GET("/api/items", cb.GetItems)
-	router.GET("/api/items/:id", cb.GetItemByID)
-	router.POST("/api/items/new", cb.NewItem)
-	router.PUT("/api/items/edit/:id", cb.EditItem)
-	router.DELETE("/api/items/delete/:id", cb.DeleteItem)
+	router.GET("/api/items", stdChain.Then(cb.GetItems))
+	router.GET("/api/items/:id", stdChain.Then(cb.GetItemByID))
+	router.POST("/api/items/new", stdChain.Then(cb.NewItem))
+	router.PUT("/api/items/edit/:id", stdChain.Then(cb.EditItem))
+	router.DELETE("/api/items/delete/:id", stdChain.Then(cb.DeleteItem))
 	router.GET("/api/users", (stdChain.Then(cb.GetUsers)))
-	//router.GET("/api/users", cb.GetUsers)
 	router.GET("/api/users/:id", stdChain.Then(cb.GetUser))
-	//router.GET("/api/users/:id", cb.GetUser)
 	router.PUT("/api/users/edit/:id", stdChain.Then(cb.UpdateUser))
-	router.DELETE("/api/users/delete/:id", cb.DeleteUser)
-	//router.PUT("/api/users/edit/:id", cb.UpdateUser)
-
 	router.HandlerFunc("POST", "/api/users/register", cb.CorkboardAuth.RegisterUser())
 	router.HandlerFunc("POST", "/api/users/auth", cb.CorkboardAuth.AuthUser())
 
