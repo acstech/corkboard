0285648c905154621065b26235edb2ad41c403dc
Ben Wornom <bworn621@students.bju.edu>
2017-07-06 16:19:20 -0400

    testing linter

--- userhandles.go
+++ userhandles.go
@@ -213,15 +213,15 @@ func (cb *Corkboard) DeleteUser(w http.ResponseWriter, r *http.Request, ps httpr
 		return
 	}
 
-	items, _ := cb.findUserItems(id)
-
-	for i := 0; i < len(items); i++ {
-		var docID = "item:" + items[i].ID
-		_, err := cb.Bucket.Remove(docID, 0)
-		if err != nil {
-			log.Println(err)
-		}
-	}
+	// items, _ := cb.findUserItems(id)
+	//
+	// for i := 0; i < len(items); i++ {
+	// 	var docID = "item:" + items[i].ID
+	// 	_, err := cb.Bucket.Remove(docID, 0)
+	// 	if err != nil {
+	// 		log.Println(err)
+	// 	}
+	// }
 
 	w.WriteHeader(http.StatusOK)
 
