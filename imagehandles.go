615c576a2891a226ef33da1d85e8c6916b09896a
jasonm <jason.moore2@furman.edu>
2017-07-13 16:40:57 -0400

    Not worrying about contentmd5 or content type for now

--- imagehandles.go
+++ imagehandles.go
@@ -75,9 +75,9 @@ func imageURL(w http.ResponseWriter, r *http.Request) {
 		Bucket: aws.String(os.Getenv("CB_S3_BUCKET")),
 		Key:    aws.String(key),
 	})
-	checksum := picID.Checksum
-	req.HTTPRequest.Header.Set("Content-MD5", checksum)
-	req.HTTPRequest.Header.Set("Content-Type", fmt.Sprintf("image/%s", imageExtension))
+	//checksum := picID.Checksum
+	// req.HTTPRequest.Header.Set("Content-MD5", checksum)
+	// req.HTTPRequest.Header.Set("Content-Type", fmt.Sprintf("image/%s", imageExtension))
 	url, err := req.Presign(15 * time.Minute)
 	if err != nil {
 		log.Println(err)
