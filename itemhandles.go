9d7bf751a593076932106c215a7eac4ccf0a6dbf
jasonm <jason.moore2@furman.edu>
2017-07-24 11:33:55 -0400

    Parsing prices

--- itemhandles.go
+++ itemhandles.go
@@ -183,7 +183,10 @@ func (corkboard *Corkboard) EditItem(w http.ResponseWriter, r *http.Request, p h
 		priceSplit = strings.Replace(priceSplit, ",", "", -1)
 		price, err = strconv.ParseFloat(priceSplit, 64)
 		if err != nil {
-			log.Println(err)
+			errs.Errors = append(errs.Errors, ErrorRes{Message: "Parsing price failed. Allowed characters: $ , . 0-9"})
+			errsRes, _ := json.Marshal(errs)
+			w.WriteHeader(http.StatusBadRequest)
+			w.Write(errsRes)
 			return
 		}
 	} else if priceSplit == "0.00" || reqitem.Price == "" {
